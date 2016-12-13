c638bfc05a9160766bbbd5b215d64feb7d5bc13d
Atsunori Koura <ak_box2@yahoo.co.jp>
2016-12-13 14:30:41 +0000

    lesson7 kadai1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,11 @@
 module SessionsHelper
+  
+  #現在ログインしているユーザーを返す（ユーザーがログイン中の場合のみ）
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])  
   end
   
+  #ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !!current_user
   end
